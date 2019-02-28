529380946add1059d0b9881f40dcbd339ce97f91
Trent Moss <trentmoss05@gmail.com>
2019-02-28 23:13:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,5 +12,14 @@ module FormsLab
       erb :'pirates/new'
     end
 
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+      erb :'pirates/show'
+    end
+
   end
 end
